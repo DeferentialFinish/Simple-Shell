9d378d8d20a4bca0ee7b8dfb62dc67c380d15c92
Exylum Technical <94145193+ExylumTechnical@users.noreply.github.com>
2023-04-09 14:26:45 -0700

    Create ascii.cpp

---
+++ ascii.cpp
@@ -0,0 +1,22 @@
+
+/*
+ Program created by Nicholas Howland
+ Date: 2023-01-17
+ Program purpose: Ascii Art
+*/
+#include <iostream> // include the iostream library/module
+using namespace std; // defines the context of the namespace before stdin/cout
+
+int main() // This is the function that will always run when the program is
+{ // start of main function
+        cout << "ASCII - American Standard Code for Information Interchange" << endl;
+        cout << "ASCII Art" << endl;
+cout << "    ______           __                   ______          __  "<< endl;"<< endl;
+cout << "   / ____/  ____  __/ /_  ______ ___     /_  __/__  _____/ /_ "<< endl;"<< endl;
+cout << "  / __/ | |/_/ / / / / / / / __ `__ \\     / / / _ \\/ ___/ __ \\"<< endl;"<< endl;
+cout << " / /____>  </ /_/ / / /_/ / / / / / /    / / /  __/ /__/ / / /"<< endl;"<< endl;
+cout << "/_____/_/|_|\\__, /_/\\__,_/_/ /_/ /_/    /_/  \\___/\\___/_/ /_/ "<< endl;"<< endl;
+cout << "           /____/                                             "<< endl;"<< endl;
+
+
+} // end of main function
